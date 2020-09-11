0905ad776e9b6b4dfb39224327b84cdc4de20177
王世安 <984292420@qq.com>
2020-09-11 14:46:01 +0800

    feat: mask 修改

--- src/editor/core/mask.svelte
+++ src/editor/core/mask.svelte
@@ -3,3 +3,41 @@
    *
    */
 </script>
+
+
+<style>
+    .mask{
+        opacity: 0;
+        background: #000;
+        position: fixed;
+        left:0;
+        right:0;
+        top:0;
+        bottom:0;
+        z-index: 10001;
+        transition: opacity 0.2s;
+    }
+    .mask-show{
+        opacity: 0.2;
+    }
+
+    .process{
+        position: fixed;
+        left:50%;
+        top:50%;
+        width:200px;
+        height:30px;
+        line-height: 30px;
+        background: #fff;
+        margin-top: -15px;
+        margin-left: -100px;
+        z-index: 10002;
+        text-align: center;
+    }
+</style>
+
+<div>
+<!-- 添加 show  -->
+    <div class="mask" />
+    <div class="process"></div>
+</div>
